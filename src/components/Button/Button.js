c41a9762ba8d83183b77ae576fa64b77cb95872a
Levani Papashvili <papashvililevani@gmail.com>
2020-02-21 16:14:39 -0500

    new

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,12 +4,36 @@ import './Button.css';
 // This is a functional component - just sent up a little differently as an arrow function!
 const Button = (props) => {
     let classList = ''
-    let types = ["primary", "danger", "warning", "success", "lol",];
+    let types = [
+      "primary1",
+      "primary2",
+      "primary3", 
+      "primary4",
+      "primary5",
+      "primary6",
+      "primary7",
+      "danger1",
+      "danger2",
+      "danger3",
+      "danger4",
+      "danger5",
+      "danger6",
+      "danger7",
+      "warning",
+      "success",
+      "lol"
+    ];
     if (types.includes(props.type)) {
         classList += `button-${props.type}`
     }
     if (props.large) {
         classList += ` button-large`
+    }
+    if (props.border){
+        classList += ` button-border-${props.type}`
+    }
+    if (props.pale){
+        classList += ` button-pale-${props.type}`
     }
     return <button className={classList} onClick={props.onClick}> 
         {props.label}
--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -36,15 +36,37 @@ import Button from './Button';
 // };
 
 storiesOf("Button", module)
-  .add("Primary", () => <Button label="Primary" type="primary" />)
-   .add("Success", () => <Button label="Success" type="success" />)
-  .add("Danger", () => <Button label="Danger" type="danger" />)
-//   .add("Warining", () => <Button label="Warining" type="warning" />)
+  .add("Primary1", () => <Button label="Do something" type="primary1" />)
+  .add("Primary2", () => <Button label="Do something" type="primary2" />)
+  .add("Primary3", () => <Button label="Do something" type="primary3" />)
+  .add("Primary4", () => <Button label="Do something" type="primary4" />)
+  .add("Primary5", () => <Button label="Do something" type="primary5" large />)
+  .add("Primary6", () => <Button label="Do something" type="primary6" large />)
+  .add("Primary7", () => <Button label="Do something" type="primary7" large />)
+  .add("Danger1", () => <Button label="Do something" type="danger1" />)
+  .add("Danger2", () => <Button label="Do something" type="danger2" />)
+  .add("Danger3", () => <Button label="Do something" type="danger3" />)
+  .add("Danger4", () => <Button label="Do something" type="danger4" />)
+  .add("Danger5", () => <Button label="Do something" type="danger5" large />)
+  .add("Danger6", () => <Button label="Do something" type="danger6" large />)
+  .add("Danger7", () => <Button label="Do something" type="danger7" large />)
+
+  .add("Success", () => <Button label="Success" type="success" />)
+  .add("Success large", () => <Button label="Success" type="success" large />)
+  .add("Success border", () => <Button label="Success" type="success" border />)
+  .add("Success border large", () => <Button label="Success" type="success" border large />)
+  .add("Success  pale", () => <Button label="Success" type="success" pale  />)
+  .add("Success pale  large", () => <Button label="Success" type="success" pale large />)
+  
+
+  //   .add("Warining", () => <Button label="Warining" type="warning" />)
   .add("Large Primary", () => (
     <Button label="Large Primary Button" type="primary" large />
   ))
-  .add("Large Danger", () => <Button label="Large Danger Button" type="danger" large />)
-  .add('LOLO', () => <Button label="lol" type="lol"/>)
+  .add("Large Danger", () => (
+    <Button label="Large Danger Button" type="danger" large />
+  ))
+  .add("LOLO", () => <Button label="lol" type="lol" />);
 //   LOLO IS THE BUTTON NAME OF THE BUTTON
 //   LABEL IS THE TEXT INSIDE OF THE BUTTON 
 //   TYPE IS NAME OF THE CSS STYLING
