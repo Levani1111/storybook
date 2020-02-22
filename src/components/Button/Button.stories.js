230f8267f9e75b293bcad2c0dbbbc01a2fd49832
Levani Papashvili <papashvililevani@gmail.com>
2020-02-22 08:36:45 -0500

    new updates

--- src/components/Button/Button.stories.js
+++ src/components/Button/Button.stories.js
@@ -3,37 +3,7 @@ import { storiesOf } from '@storybook/react';
 
 import Button from './Button';
 
-// const style = {
-//   backgroundColor: "#0069D9",
-//   color: "white",
-//   borderRadius: "2px",
-//   padding: "5px 25px",
-//   border: "#0069D9"
-// };
 
-// const successStyle = {
-//   backgroundColor: "red",
-//   color: "white",
-//   borderRadius: "2px",
-//   padding: "5px 25px",
-//   border: "black"
-// };
-
-//const Danger = {
-  //backgroundColor: "green",
-  //color: "white",
-  //borderRadius: "2px",
-  //padding: "5px 25px",
-  //border: "black"
-//};
-
-// const warningStyle = {
-//   backgroundColor: "black",
-//   color: "white",
-//   borderRadius: "2px",
-//   padding: "5px 25px",
-//   border: "black"
-// };
 
 storiesOf("Button", module)
   //----------------------------------------------------------------------------------
@@ -85,21 +55,11 @@ storiesOf("Button", module)
   .add("Default large", () => <Button label="Default" type="Default" large />)
   .add("Default border", () => <Button label="Default" type="Default" border />)
   .add("Default border large", () => (
-    <Button label="Success" type="success" border large />
+    <Button label="Default" type="Default" border large />
   ))
   .add("Default  pale", () => <Button label="Default" type="Default" pale />)
   .add("Default pale  large", () => (
     <Button label="Default" type="Default" pale large />
   ));
   //---------------------------------------------------------------------------------------------
-  //   .add("Warining", () => <Button label="Warining" type="warning" />)
-  //.add("Large Primary", () => (
-  // <Button label="Large Primary Button" type="primary" large />
-  //))
-  //.add("Large Danger", () => (
-    //<Button label="Large Danger Button" type="danger" large />
-  //));
-  //.add("LOLO", () => <Button label="lol" type="lol" />);
-//   LOLO IS THE BUTTON NAME OF THE BUTTON
-//   LABEL IS THE TEXT INSIDE OF THE BUTTON 
-//   TYPE IS NAME OF THE CSS STYLING
+  
