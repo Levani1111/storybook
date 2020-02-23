972f0ec59b940f6c060aae32b21482a62df3c4e8
Levani Papashvili <papashvililevani@gmail.com>
2020-02-22 21:38:36 -0500

    Counter

---
+++ src/components/Counter.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+
+export class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: this.props.value
+    };
+  }
+  increase = () => {
+    if (this.state.number + this.props.step <= this.props.max) {
+      if (this.props.step) {
+        let newStep = this.props.step;
+        let count = this.state.number + newStep;
+        this.setState({ number: count });
+      } else {
+        let newCount = this.state.number + 1;
+        this.setState({ number: newCount });
+      }
+    }
+  };
+
+  decrease = () => {
+    if (this.state.number - this.props.step >= this.props.min) {
+      if (this.props.step) {
+        let newStep = this.props.step;
+        let count = this.state.number - newStep;
+
+        this.setState({ number: count });
+      } else {
+        let newCount = this.state.number - 1;
+        this.setState({ number: newCount });
+      }
+    }
+  };
+  render() {
+    return (
+      <div style={counterStyle}>
+        <button style={buttonStyle} onClick={this.decrease}>
+          -
+        </button>
+        <div>{this.state.number}</div>
+        <button style={buttonStyle} onClick={this.increase}>
+          +
+        </button>
+      </div>
+    );
+  }
+}
+export default Counter;
+
+const counterStyle = {
+  backgroundColor: "#F6F7F8",
+  display: "flex",
+  flexDirection: "row",
+  width: "116px",
+  height: "46px",
+  color: "#262626",
+  alignItems: "center",
+  justifyContent: "space-around",
+  fontSize: "20px",
+  borderRadius: "4px"
+};
+const buttonStyle = {
+  backgroundColor: "#F6F7F8",
+  color: "#33A0FF",
+  height: "46px",
+  width: "25px",
+  textAlign: "center",
+  padding: "0"
+};
---
+++ src/components/Counter.stories.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+
+import Counter from "./Counter.js";
+
+storiesOf("Counter", module).add("Counter", () => (
+  <Counter max={99} min={-99} value={1} step={1} />
+));
