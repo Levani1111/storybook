07d8c36d03afddbfeff9a57261039f138b02529f
Levani Papashvili <papashvililevani@gmail.com>
2020-02-23 10:15:15 -0500

    changed

--- src/components/TextInputButton/TextInputButton.js
+++ src/components/TextInputButton/TextInputButton.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Form from "../Forms/Form.js";
 import "./TextInputButton.css";
-
+// ------------------------------------------------
 export default function TextInputButton(props) {
   if (props.medium) {
     return (
