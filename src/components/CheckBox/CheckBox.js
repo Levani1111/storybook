d6e0ba20e71277340e041790795bf5d7ec6f1455
Levani Papashvili <papashvililevani@gmail.com>
2020-02-24 17:32:18 -0500

    Done

--- src/components/CheckBox/CheckBox.js
+++ src/components/CheckBox/CheckBox.js
@@ -1,63 +1,36 @@
-// import React from "react";
-// import "./CheckBox.css";
-// import checkedBlack2 from "./img/checkedBlack2.svg";
-// import checkboxblack from "./img/checkboxblack.svg";
-// import checkboxblue2 from "./img/checkboxblue2.svg";
-// import checkedblue from  "./img/checkedblue.svg";
-// // --------------------------------------------------------
-// class CheckBox extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       checked: this.props.checked,
-//       src: this.props.blue
-//         ? { uncheckedBlue }.uncheckedBlue
-//         : { uncheckedBlack }.uncheckedBlack
-//     };
-//     this.handleClick = this.handleClick.bind(this);
-//   }
-//   handleClick(e) {
-//     e.preventDefault();
-//     let newSrc;
-//     if (e.target.src.includes("checkboxblue2")) {
-//       newSrc = { checkboxblue2 }.checkboxblue2;
-//       this.setState({
-//         checked: !this.state.checked,
-//         src: newSrc
-//       });
-//     } else if (e.target.src.includes("checkedblue")) {
-//              newSrc = { checkedblue }.checkedblue;
-//              this.setState({
-//                checked: !this.state.checked,
-//                src: newSrc
-//              });
-//            } else if (e.target.src.includes("checkedBlack2")) {
-//                     newSrc = { checkedBlack2 }.checkedBlack2;
-//                     this.setState({
-//                       checked: !this.state.checked,
-//                       src: newSrc
-//                     });
-//                   } else if (e.target.src.includes("checkboxblack")) {
-//                            newSrc = { checkboxblack }.checkboxblack;
-//                            this.setState({
-//                              checked: !this.state.checked,
-//                              src: newSrc
-//                            });
-//                          }
-//   }
-//   render() {
-//     return (
-//       <div className="checkbox">
-//         <img
-//           className="checkbox-button"
-//           src={this.state.src}
-//           alt="checkbox button"
-//           onClick={this.handleClick}
-//         />
-//         <p className="checkbox-label">{this.props.label}</p>
-//       </div>
-//     );
-//   }
-// }
+import React, { Component } from "react";
+import "./CheckBox.css";
+import  checkbox from "../../img/checkboxblue.svg";
 
-// export default CheckBox;
+class CheckBox extends Component{
+    constructor(props){
+        super(props)
+        this.state ={
+            isonclick:true
+        }
+    }
+    box = ()=>{
+        if (this.state.isonclick === true){
+            this.setState({isonclick:false})
+        } else {
+            this.setState({isonclick:true})
+        }
+    }
+    render(){
+        if (this.state.isonclick == true){
+            return(
+            <div onClick={this.box} className="checkbox" > <img className="img" src={checkbox}/> </div>
+        )
+        }
+
+        if (this.state.isonclick == false){
+            return(
+                <div onClick={this.box} className="checkbox" >  </div>
+            )
+        }
+        
+    }
+}
+
+
+export default CheckBox;
--- src/components/CheckBox/CheckBox.stories.js
+++ src/components/CheckBox/CheckBox.stories.js
@@ -3,7 +3,8 @@ import { storiesOf } from "@storybook/react";
 import CheckBox from "./CheckBox";
 // ----------------------------------------------------------
 storiesOf("Check Box", module)
-  .add("Black CheckBox", () => <CheckBox checked={false} />)
+  
+ .add("Black CheckBox", () => <CheckBox checked={false}  />)
   .add("Blue CheckBox", () => <CheckBox checked={false} blue />)
   .add("Black CheckBox w/Label", () => (
     <CheckBox checked={false} label="Don't show this popup again" />
