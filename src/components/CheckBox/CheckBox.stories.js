b786b06ec0204f94e956cc329e60754f4104ec03
Levani Papashvili <papashvililevani@gmail.com>
2020-03-26 20:07:36 -0400

    edited stories

--- src/components/CheckBox/CheckBox.stories.js
+++ src/components/CheckBox/CheckBox.stories.js
@@ -5,5 +5,5 @@ import Checkbox from "./CheckBox";
 storiesOf("Check Box", module)
   .add("Black CheckBox", () => <Checkbox type="Checkbox" default />)
   .add("Blue CheckBox", () => <Checkbox type="Checkbox" blue />)
-  .add("Black CheckBox w/Label", <Checkbox type="Checkbox" label="Label" defaultText />);
+  .add("Black CheckBox w/Label", <Checkbox type="Checkbox" label="Label" />)
   
