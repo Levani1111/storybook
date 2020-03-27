b2ef3418742718e2bd66f480b4c6407690d6bbf5
Levani Papashvili <papashvililevani@gmail.com>
2020-03-26 20:00:37 -0400

    debugged

--- src/components/CheckBox/CheckBox.stories.js
+++ src/components/CheckBox/CheckBox.stories.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import CheckBox from "./CheckBox";
+import Checkbox from "./CheckBox";
 // ----------------------------------------------------------
 storiesOf("Check Box", module)
   
